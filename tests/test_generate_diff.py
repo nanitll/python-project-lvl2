ab3c665c36ad450c0c119bf08fe1278f9295a139
nanitll <pasha-s98@ya.ru>
2021-04-09 15:44:24 +0300

    rename test_generate

--- tests/test_generate_diff.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from gendiff.scripts.gendiff import generate_diff
-
-def test_generate_diff():
-    assert generate_diff("tests/fixtures/file1.json",
-                         "tests/fixtures/file2.json") == " - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n"
