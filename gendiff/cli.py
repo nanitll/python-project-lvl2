37d6d73d437982208c78f1e1ff0a381c25d5d192
nanitll <pasha-s98@ya.ru>
2021-04-14 19:09:22 +0300

    move argparse in cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="stylish"
+        help="set format of output",
+        metavar="FORMAT",
+        dest="format",
+        )
+    return parser.parse_args()
