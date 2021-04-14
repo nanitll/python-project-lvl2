5bcbd1ae68e38fd7425c404d89c88e07a975d9f6
nanitll <pasha-s98@ya.ru>
2021-04-14 19:10:54 +0300

    error module

---
+++ gendiff/error_constants.py
@@ -0,0 +1,8 @@
+FORMAT_ERROR = """Invalid output format: '{0}'.
+Try 'json', 'plain' or 'stylish'."""
+
+VALUE_ERROR = """Unsupported file extension: '{0}'
+'.json' and '.yaml' are supported."""
+
+NOT_FOUND_ERROR = """File not found.
+No such file or directory: '{0}/tests/fixtures/nonexistent.json'"""
