6269897ca1b50c77faa502abd7b7fea698af89b9
nanitll <pasha-s98@ya.ru>
2021-04-14 19:15:01 +0300

    move to separate modules

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,26 @@
 #!usr/bin/env python3
 import argparse
 import json
+import yaml
 
 
 def output_diff(key1, key2, operand=' '):
     return " {} {}: {}\n".format(operand, key1, key2)
 
 
+def pars_files(file_path1, file_path2):
+    print(file_path1[-1:-3:-1])
+    if file_path1[-4::] == 'yaml' and file_path2[-4::] == 'yaml':
+        file_1 = yaml.safe_load(open(file_path1))
+        file_2 = yaml.safe_load(open(file_path2))
+    else:
+        file_1 = json.load(open(file_path1))
+        file_2 = json.load(open(file_path2))
+    return file_1, file_2
+
+
 def generate_diff(file_path1, file_path2):
-    file_1 = json.load(open(file_path1))
-    file_2 = json.load(open(file_path2))
+    file_1, file_2 = pars_files(file_path1, file_path2)
     diff = []
 
     for i in file_1.keys():
@@ -30,7 +41,7 @@ def generate_diff(file_path1, file_path2):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate diff")
+    parser = argparse.ArgumentParser(description="Compares two cnfiguration files and shows a diference")
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
     parser.add_argument("-f", "--format", help="set format of output")
